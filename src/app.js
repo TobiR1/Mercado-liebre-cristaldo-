e631f50f9dd21faf4435dea2e7d9c8d19748ffed
TobiR1 <tobiasramirezv6@gmail.com>
2023-08-21 20:51:25 -0300

    Mercado liebre

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../public")));
+
+const PORT = 3001;
+app.listen(PORT, ()=>{
+    console.log('Se prendio en el puerto  ${PORT}');
+});
+app.get("/", (req,res)=>{
+    res.sendFile (path.join(__dirname,"views/home.html"))
+ });
+
+app.post("/login", (req, res) => {
+  res.sendFile (path.resolve(__dirname,"views/login.html"))
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/register.html"));
+  });
